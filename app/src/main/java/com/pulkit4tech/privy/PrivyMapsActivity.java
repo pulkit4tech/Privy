36556e5cd1712d7b747fb0b9b983ded84a490575
Pulkit Bhatia <pulkit4tech@gmail.com>
2016-12-24 19:13:05 +0530

    Some Improvement

--- app/src/main/java/com/pulkit4tech/privy/PrivyMapsActivity.java
+++ app/src/main/java/com/pulkit4tech/privy/PrivyMapsActivity.java
@@ -1,18 +1,17 @@
 package com.pulkit4tech.privy;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PrivyMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class PrivyMapsActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -31,7 +30,7 @@ public class PrivyMapsActivity extends FragmentActivity implements OnMapReadyCal
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near Delhi.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
